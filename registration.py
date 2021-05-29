43948604b1e935f1eda1fc9a115c5954adf23039
surajtiwaari12 <84776481+surajtiwaari12@users.noreply.github.com>
2021-05-29 12:18:01 +0530

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,68 @@
+<html>
+<head>
+	<title>Register form</title>
+</head>
+<body>
+<form>
+<center>
+<h2>Register form</h2>
+<table>
+<tr>
+	<td>
+		Name:
+	</td>
+	<td>
+		<input type="text" placeholder="Name" name="">
+	</td>
+	<tr>
+	<td>
+		password:
+	</td>
+	<td>
+		<input type="password" placeholder="password" name="">
+	</td>
+</tr>
+<tr>
+<td>
+	Gender:
+</td>
+<td>
+	<input type="radio" name="Gender">Male
+	<input type="radio" name="Gender">Female
+</td>
+</tr>
+<tr>
+	<td>
+		Email:
+	</td>
+	<td>
+		<input type="mail" placeholder="Email" name="">
+	</td>
+</tr>
+<tr>
+	<td>
+		Phone no:
+	</td>
+	<td>
+		<select>
+			<option>877</option>
+			<option>878</option>
+			<option>879</option>
+			<option>880</option>
+			<option>881</option>
+			<option>882</option>
+			<option>883</option>
+		</select>
+		<input type="Phone" placeholder="96554***" name="">
+	</td>
+</tr>
+
+<tr>
+	<td>
+		<input type="submit" value="submit" name="">
+	</td>
+</tr>
+</table>
+</center>
+</body>
+</html>
